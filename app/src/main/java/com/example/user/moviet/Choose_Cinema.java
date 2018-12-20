a5cdee0abef65bd7a2339d1f4ed2da160947b30e
avital477 <avital477@gmail.com>
2018-12-20 21:33:03 +0200

    second commit

--- app/src/main/java/com/example/user/moviet/Choose_Cinema.java
+++ app/src/main/java/com/example/user/moviet/Choose_Cinema.java
@@ -6,47 +6,130 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class Choose_Cinema extends AppCompatActivity {
-    ListView listview;
+    // Wigets - GUI
+    Spinner spCountries;
+    Spinner spBusinessType;
+
+    // Data Source
+
+
+    // Adapter
+//    ArrayAdapter<String> adapterBusinessType;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose__cinema);
 
-        listview = (ListView)findViewById(R.id.listview);
-        final ArrayList<String> ListOfCinemas = new ArrayList <> ();
-        ListOfCinemas.add("Tel Aviv");
-        ListOfCinemas.add("Petach Tikva");
-        ListOfCinemas.add("Ariel");
-        ListOfCinemas.add("Itzhar");
-        ListOfCinemas.add("Haifa");
-        ListOfCinemas.add("Jerusalem");
-        ListOfCinemas.add("Beer-Sheva");
-//        ListOfCinemas.add("Hispin");
-//        ListOfCinemas.add("Katzrin");
-//        ListOfCinemas.add("Tzfat");
+        // Initialize Spinners
 
+        spCountries = (Spinner) findViewById(R.id.spCountries);
+        spBusinessType = (Spinner) findViewById(R.id.spBussinessType);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,ListOfCinemas);
-        listview.setAdapter(arrayAdapter);
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Initialize and set Adapter
+//        adapterBusinessType = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, businessType);
+//        spBusinessType.setAdapter(adapterBusinessType);
+
+        // Country Item Selected Listener
+        spCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
-            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+            public void onItemSelected(AdapterView<?> adapter, View v, int position, long id) {
+                // On selecting a spinner item
+                String item = adapter.getItemAtPosition(position).toString();
 
-                Intent myIntent = new Intent(getBaseContext(), Choose_Date.class);
-                startActivity(myIntent);
-
-                //  Toast.makeText(Choose_Cinema.this, "hannnniiii" + position + " " + ListOfCinemas.get(position).toString(),Toast.LENGTH_SHORT).show();
+                // Showing selected spinner item
+                Toast.makeText(getApplicationContext(), "Selected Country : " + item, Toast.LENGTH_LONG).show();
             }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
 
+            }
+        });
+
+
+        // Business Type Item Selected Listener
+        spBusinessType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View v, int position, long id) {
+                // On selecting a spinner item
+                String item = adapter.getItemAtPosition(position).toString();
+
+                // Showing selected spinner item
+                Toast.makeText(getApplicationContext(), "Bussiness Type : " + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+        });
+        Button button = findViewById(R.id.button10);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent (Choose_Cinema.this, Choose_Date.class);
+                startActivity(intent);
+            }
         });
 
     }
 
+
 }
+
+
+//
+//    ListView listview;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_choose__cinema);
+//
+//        listview = (ListView)findViewById(R.id.listview);
+//        final ArrayList<String> ListOfCinemas = new ArrayList <> ();
+//        ListOfCinemas.add("Tel Aviv");
+//        ListOfCinemas.add("Petach Tikva");
+//        ListOfCinemas.add("Ariel");
+//        ListOfCinemas.add("Itzhar");
+//        ListOfCinemas.add("Haifa");
+//        ListOfCinemas.add("Jerusalem");
+//        ListOfCinemas.add("Beer-Sheva");
+////        ListOfCinemas.add("Hispin");
+////        ListOfCinemas.add("Katzrin");
+////        ListOfCinemas.add("Tzfat");
+//
+//
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,ListOfCinemas);
+//        listview.setAdapter(arrayAdapter);
+//        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView <?> parent, View view, int position, long id) {
+//
+//                Intent myIntent = new Intent(getBaseContext(), Choose_Date.class);
+//                startActivity(myIntent);
+//
+//                //  Toast.makeText(Choose_Cinema.this, "hannnniiii" + position + " " + ListOfCinemas.get(position).toString(),Toast.LENGTH_SHORT).show();
+//            }
+//
+//
+//        });
+//
+//    }
+//
+//}
